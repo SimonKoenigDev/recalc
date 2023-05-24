9e8183e5de5aa34d4c9f4b42f2821e1fb9182f42
Santiago Di Camillo <dicamillosantiago@gmail.com>
2023-05-24 12:10:26 -0300

    Agrego test que comprueba que se guarde un error

--- test/models.test.js
+++ test/models.test.js
@@ -28,3 +28,26 @@ describe("History", () => {
         expect(histories[0].Operation.name).toEqual("SUB")
     })
 })
+
+describe("History", () => {
+    test("Deberia poder cargar un erorr en la tabla History", async () => {
+        await createHistoryEntry({
+            firstArg: 2,
+            secondArg: null,
+            result: null,
+            error: "El segundo parámetro es null",
+            operationName: "SUB"
+        })
+
+        const histories = await History.findAll({
+            include: [Operation]
+        })
+
+        expect(histories.length).toEqual(1)
+        expect(histories[0].firstArg).toEqual(2)
+        expect(histories[0].secondArg).toBeNull()
+        expect(histories[0].result).toBeNull()
+        expect(histories[0].error).not.toBeNull()
+        expect(histories[0].Operation.name).toEqual("SUB")
+    })
+})
