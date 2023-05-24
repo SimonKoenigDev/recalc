e8c50354c8e1ac125b271465c28a6bb27b415ff0
Santiago Di Camillo <dicamillosantiago@gmail.com>
2023-05-24 17:22:18 -0300

    Agrego test para comprobar resultado con decimales en mul

--- test/api.test.js
+++ test/api.test.js
@@ -19,3 +19,17 @@ describe("API substract", () => {
             });
     });
 })
+
+describe("API multiply", () => {
+    test("Deberia responder con un 200", async () => {
+        const app = await api.build()
+
+        return request(app)
+            .get('/api/v1/mul/2.1/2.1')
+            .expect(200)
+            .expect('Content-Type', "application/json; charset=utf-8")
+            .then((res) => {
+                expect(res.body.result).toEqual(4.41);
+            });
+    });
+})
