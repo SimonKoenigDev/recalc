e3e2043771dbff6f15253f7901bcef792e3833a3
Enzo Defranco <enzolobod22@gmail.com>
2023-05-24 17:15:45 -0300

    Agrego test en la operacion add

--- test/api.test.js
+++ test/api.test.js
@@ -10,13 +10,30 @@ describe("API substract", () => {
     test("Deberia responder con un 200 ok", async () => {
         const app = await api.build()
 
-        request(app).get('/api/v1/sub/2/1')
+        return request(app)
+            .get('/api/v1/sub/2/1')
             .expect(200)
             .expect('Content-Type', "application/json; charset=utf-8")
-            .end((err, res) => {
-                if (err) throw err
-
+            .then((res) => {
                 expect(res.body.result).toEqual(1);
-            })
-    })
+            });
+    });
 })
+
+//prove that if the second parameter is negative, the result is less than the first parameter
+
+describe("API add", () => {
+    test("Si segundo parámetro es negativo, el resultado tiene que sermenor al primer parámetro y el endpoint devuelver un status 200.", async () => {
+        const app = await api.build()
+
+        return request(app)
+        .get('/api/v1/add/2/-1')
+        .expect(200) // Espera un código de estado 200
+        .expect('Content-Type', "application/json; charset=utf-8") // Espera el tipo de contenido adecuado
+        .then((res) => {
+          const result = res.body.result;
+          expect(result).toBeLessThan(2); // Comprueba que el resultado sea menor a 2 (primer parámetro)
+        });
+    });
+});
+
