dd1187c95108da8e0bfef1b1cf3712f132d09709
Enzo Defranco <enzolobod22@gmail.com>
2023-05-24 11:02:57 -0300

    Agrego el test para segundo argumento al momento de cargar un nuevo registro en la operacion SUB

--- test/models.test.js
+++ test/models.test.js
@@ -27,4 +27,25 @@ describe("History", () => {
         expect(histories[0].result).toEqual(0)
         expect(histories[0].Operation.name).toEqual("SUB")
     })
+
+    test("Deberia poder persistir el segundo argumento de tabla History", async () => {
+        await createHistoryEntry({
+            firstArg: 24,
+            secondArg: 2,
+            result: 22,
+            operationName: "SUB"
+        })
+
+        const histories = await History.findAll({
+            include: [Operation]
+        })
+
+        expect(histories.length).toEqual(1)
+        expect(histories[0].firstArg).toEqual(24)
+        expect(histories[0].secondArg).not.toBeNull() // Si el segundo argumento es Nulo, no pasa el test.
+        expect(histories[0].result).toEqual(22)
+        expect(histories[0].Operation.name).toEqual("SUB")
+    })
+    
+    
 })
