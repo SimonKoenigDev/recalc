62d5757abf80776f6437e001fe268ee931551477
Simon Koenig <koenigg.simon@gmail.com>
2023-05-24 17:27:08 -0300

    Agrego test para verificar que el divisor sea distinto de cero

--- test/api.test.js
+++ test/api.test.js
@@ -10,13 +10,27 @@ describe("API substract", () => {
     test("Deberia responder con un 200 ok", async () => {
         const app = await api.build()
 
-        request(app).get('/api/v1/sub/2/1')
+        return request(app)
+            .get('/api/v1/sub/2/1')
             .expect(200)
             .expect('Content-Type', "application/json; charset=utf-8")
-            .end((err, res) => {
-                if (err) throw err
-
+            .then((res) => {
                 expect(res.body.result).toEqual(1);
-            })
-    })
+            });
+    });
 })
+
+describe("API divide", () => {
+    test("Debería responder con un 400 ERROR", async () => {
+        const app = await api.build();
+
+        return request(app)
+            .get('/api/v1/div/2/0')
+            .expect(400)
+            .expect('Content-Type', "application/json; charset=utf-8")
+            .then((res) => {
+                expect(res.body.message).toEqual("El divisor no puede ser cero");
+            });
+    });
+});
+
